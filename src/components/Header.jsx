151def021ea8268ca0e3a2c317e106afd5039bb0
Ryan McLean <mcleanrs@comcast.net>
2019-04-19 16:36:22 -0700

    change theme color from green to blue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,16 @@ import '../scss/styles.scss';
 function Header(){
   let margin = {
     margin: '0',
-    padding: '0'
+    padding: '0',
+    color: 'white'
+  };
+  let white = {
+    color: 'white'
   };
   return (
     <div className='bg-grn header'>
       <h1 style={margin}>Tap Room</h1>
-      <Link to="/">Home</Link> | <Link to="/newTap">New Tap</Link>
+      <a><Link style={white} to="/">Home</Link> | <Link style={white} to="/newTap">New Tap</Link></a>
     </div>
   );
 }
