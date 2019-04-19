2bec48a8221ffcff5678365b1f7d017ffa4586ae
Ryan McLean <mcleanrs@comcast.net>
2019-04-19 16:53:22 -0700

    add home.jsx for main screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,15 @@ import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 import NewTapForm from './NewTapForm';
 import Error404 from './Error404';
+import Home from './Home';
 
 function App(){
   return (
     <div>
       <Header/>
       <Switch>
-        <Route exact path='/' component={TapList} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/inventory' component={TapList}/>
         <Route exact path='/newTap' component={NewTapForm} />
         <Route component={Error404}/>
       </Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header(){
   return (
     <div className='bg-grn header'>
       <h1 style={margin}>Tap Room</h1>
-      <a><Link style={white} to="/">Home</Link> | <Link style={white} to="/newTap">New Tap</Link></a>
+      <Link style={white} to="/">Home</Link> | <Link style={white} to="/inventory">Inventory</Link> | <Link style={white} to="/newTap">New Tap</Link> 
     </div>
   );
 }
