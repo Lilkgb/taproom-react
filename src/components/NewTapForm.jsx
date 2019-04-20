691a5e5d6924c35da7f54b9ce452e82990c0edf8
Ryan McLean <mcleanrs@comcast.net>
2019-04-19 17:02:35 -0700

    remove description from form

--- src/components/NewTapForm.jsx
+++ src/components/NewTapForm.jsx
@@ -3,18 +3,24 @@ import React from 'react';
 function NewTapForm(){
   return (
     <div>
+    <style jsx>{`
+      div {
+        margin: 10px;
+      }
+      `}</style>
       <form>
         <input
           type='text'
-          id='names'
+          id='name'
           placeholder='Tap Name'/>
         <input
-          type='text'
-          id='location'
-          placeholder='Tap Alcahol Content'/>
-        <textarea
-          id='issue'
-          placeholder='Description'/>
+          type='number'
+          id='price'
+          placeholder='Price'/>
+        <input
+          type='number'
+          id='abv'
+          placeholder='Alcohol Content'/>
         <button type='submit'>Submit</button>
       </form>
     </div>
