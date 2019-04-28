e05e49afbd1483e1d1a6784e846c4fbcae961ced
lilkgb <mcleanrs@comcast.net>
2019-04-28 16:04:15 -0700

    convert newtapform back into function

--- src/components/NewTapForm.jsx
+++ src/components/NewTapForm.jsx
@@ -1,45 +1,40 @@
 import React from 'react';
 
-class NewTapForm extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-
-    }
-  }
-
-  handleClick(){
+function NewTapForm(){
+  let _name = null;
+  let _price = null;
+  let _abv = null;
+  
+  function submitForm(){
     console.log("YEET")
   }
-  render(){
-    return (
-      <div>
-        <style jsx>{`
-            div {
-              margin: 10px;
-            }
-            `}</style>
-          <form>
-            <input
-              type='text'
-              id='name'
-              placeholder='Tap Name'
-              ref={(input) => {_name = input;}}/>
-            <input
-              type='number'
-              id='price'
-              placeholder='Price'
-              ref={(input) => {_price = input;}}/>
-            <input
-              type='number'
-              id='abv'
-              placeholder='Alcohol Content'
-              ref={(input) => {_abv = input;}}/>
-            <button onClick={this.handleClick}>Submit</button>
-          </form>
-        </div>
-      );
-  }
+  return (
+    <div>
+      <style jsx>{`
+          div {
+            margin: 10px;
+          }
+          `}</style>
+      <form onSubmit={this.submitForm}>
+        <input
+          type='text'
+          id='name'
+          placeholder='Tap Name'
+          ref={(input) => {_name = input;}}/>
+        <input
+          type='number'
+          id='price'
+          placeholder='Price'
+          ref={(input) => {_price = input;}}/>
+        <input
+          type='number'
+          id='abv'
+          placeholder='Alcohol Content'
+          ref={(input) => {_abv = input;}}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewTapForm;
