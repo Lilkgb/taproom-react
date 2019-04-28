178d1a83ffe4807773d05f96d6056f27bd7e5904
lilkgb <mcleanrs@comcast.net>
2019-04-28 14:07:22 -0700

    remove text place holder in security question

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header(){
   return (
     <div className='bg-grn header'>
       <h1 style={margin}>Tap Room</h1>
-      <Link style={white} to="/">Home</Link> | <Link style={white} to="/inventory">Inventory</Link> | <Link style={white} to='/formControl'>form</Link>
+      <Link style={white} to="/">Home</Link> | <Link style={white} to="/inventory">Inventory</Link> | <Link style={white} to='/formControl'>New Tap</Link>
     </div>
   );
 }
--- src/components/SecurityQuestion.jsx
+++ src/components/SecurityQuestion.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function SecurityQuestion(){
   return(
     <div>
-      <p>blah</p>
       <form>
         <p>What is the password?</p>
         <input placeholder="password"/>
